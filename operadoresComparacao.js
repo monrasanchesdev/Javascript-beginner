9c1b66b65fc321c2608677771b94b44210cb4c94
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-02-22 15:09:08 +0000

    adicionando operadoresComparacao

---
+++ operadoresComparacao.js
@@ -0,0 +1,31 @@
+let idadeRamon = 26;
+let idadeCatherine = 28;
+let idadeMinhaFilha = 5;
+
+//operador igualdade
+const comparacao = idadeRamon == idadeCatherine;
+console.log('Ramon tem a mesma idade que Catherine ?', comparacao);
+
+//maior que
+const comparacao2 = idadeRamon > idadeCatherine;
+console.log('Ramon é mais velho que Catherine ?', comparacao2);
+
+//menor que
+const comparacao3 = idadeRamon < idadeCatherine;
+console.log('Ramon é mais novo que Catherine ?', comparacao3);
+
+//maior ou igual a
+const comparacao4 = idadeRamon >= idadeCatherine;
+console.log('Ramon tem a mesma idade ou mais que Catherine ?', comparacao4);
+
+//menor ou igual a
+const comparacao5 = idadeRamon <= idadeCatherine;
+console.log('Ramon tem a mesma idade ou menos que Catherine ?', comparacao5);
+//igual ou maior
+const souMaiorDeIdade = idadeRamon >= 18;
+console.log('Sou maior de idade ? ', souMaiorDeIdade);
+
+//igual ou menor
+const minhaFilhaEhMenorDeIdade = idadeMinhaFilha <= 18;
+console.log('Minha filha é menor de idade ? ', minhaFilhaEhMenorDeIdade);
+
