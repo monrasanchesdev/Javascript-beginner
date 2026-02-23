417fe8994be6c926753132bdbad513a9a6dfe4d5
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-02-23 13:27:58 +0000

    adicionando desafio

---
+++ desafio.js
@@ -0,0 +1,11 @@
+console.log('Boas vindas a calculadora de troco!');
+
+let valorCompra = 17.50;
+let valorPago = 20;
+
+let troco = valorPago - valorCompra;
+
+console.log('O valor da compra é: R$ ', valorCompra);
+console.log('O valor pago é: R$ ', valorPago);
+
+console.log('O valor do troco é: R$ ', troco);
