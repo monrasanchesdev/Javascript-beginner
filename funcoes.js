5a6dcbbf38abb3dcf586643a5de59e32f95ebb40
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-02-26 14:50:41 +0000

    adicionando funcoes

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function saudacao(nome) { // nome é um parametro da função saudacao
+    console.log('Olá, ' , nome)
+}
+
+saudacao('Ramon') // Ramon é o "argumento"
+saudacao('Catherine')
+saudacao('Maria')
+
+function calcularDobroDeUm(numero) {
+    return numero * 2
+}
+
+const numeroDobrado = calcularDobroDeUm(5)
+console.log('O dobro de 5 é:' , numeroDobrado)
+
