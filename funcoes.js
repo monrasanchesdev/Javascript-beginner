453ab44568ccb019591502e2a2df4062df459ca5
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-03-02 17:13:17 +0000

    att funcoes

--- funcoes.js
+++ funcoes.js
@@ -1,15 +1,26 @@
-function saudacao(nome) { // nome é um parametro da função saudacao
-    console.log('Olá, ' , nome)
-}
+// function saudacao(nome) { // nome é um parametro da função saudacao
+//     console.log('Olá, ' , nome)
+// }
+// saudacao('Ramon') // Ramon é o "argumento" 
 
-saudacao('Ramon') // Ramon é o "argumento"
+// const saudacao = (nome) => {
+//    console.log('Vida longa e próspera,', nome)
+// }
+
+const saudacao = nome => console.log('Vida longa e próspera,', nome)
+
+saudacao('Ramon')
 saudacao('Catherine')
 saudacao('Maria')
 
-function calcularDobroDeUm(numero) {
-    return numero * 2
+// function calcularDobroDeUm(numero) {
+//    return numero * 2
+// }
+
+const calcularDobroDeUm = (numero) => {
+      return numero * 2
 }
 
 const numeroDobrado = calcularDobroDeUm(5)
-console.log('O dobro de 5 é:' , numeroDobrado)
+    console.log('O dobro de 5 é:' , numeroDobrado)
 
