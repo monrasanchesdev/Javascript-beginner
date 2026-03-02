5fe0b3807e1fa02b6c66a4b85d887888771d5a0d
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-03-02 17:22:23 +0000

    mod funcoes

--- funcoes.js
+++ funcoes.js
@@ -1,25 +1,40 @@
+// Sintaxe Function 
+
 // function saudacao(nome) { // nome é um parametro da função saudacao
 //     console.log('Olá, ' , nome)
 // }
 // saudacao('Ramon') // Ramon é o "argumento" 
 
+// Sintaxe Arrow Function - Jeito mais moderno de escrever funções
+
 // const saudacao = (nome) => {
 //    console.log('Vida longa e próspera,', nome)
 // }
 
+
+
+// usamos essa sintaxe quando temos funções mais simples, as vezes só tem uma única instrução, ou um único parametro
 const saudacao = nome => console.log('Vida longa e próspera,', nome)
 
 saudacao('Ramon')
 saudacao('Catherine')
 saudacao('Maria')
 
+// Sintaxe Function
+
 // function calcularDobroDeUm(numero) {
 //    return numero * 2
 // }
 
-const calcularDobroDeUm = (numero) => {
-      return numero * 2
-}
+// Sintaxe Arrow Function
+
+// const calcularDobroDeUm = (numero) => {
+//      return numero * 2
+// }
+
+// Sintaxe função simples
+
+const calcularDobroDeUm = numero => numero * 2
 
 const numeroDobrado = calcularDobroDeUm(5)
     console.log('O dobro de 5 é:' , numeroDobrado)
