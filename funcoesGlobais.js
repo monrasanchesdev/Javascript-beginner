c9589a6866bcbd413e8f6c0d25d552d3dcb4361c
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-03-05 14:52:26 +0000

    at funcoesGlobais

--- funcoesGlobais.js
+++ funcoesGlobais.js
@@ -6,7 +6,10 @@ setTimeout(saudacao, 1000) // 1000ms = 1 segundo
 
 let contador = 0
 
-setInterval(() => {
+const id = setInterval(() => {
     contador++
-    console.log('Tempo decorrido:' ,contador)
+    console.log('Vou contar até 10:' ,contador)
+    if (contador == 10) {
+        clearInterval(id)
+    }
 }, 1000)
