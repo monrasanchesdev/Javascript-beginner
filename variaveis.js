f003c5dae4323d905a9ea251e27d2435f7f396e1
Ramon Leão <monrasanchesl.dev@gmail.com>
2026-03-09 15:56:18 +0000

    adicionando variaveis

---
+++ variaveis.js
@@ -0,0 +1,23 @@
+// formas de criar variáveis :
+// 1 - variável global
+// 2 - var
+// 3 - let
+// 4 - const
+
+// essa é uma variável que guarda o valor da minha idade atual 
+let idade = 26
+
+// a data que eu faço aniversário 
+const dataAniversario = '12 de março'
+
+// imprimindo a minha idade atual no console
+console.log('Minha idade atual é : ', idade)
+
+//  imprimindo minha data de aniversário no console
+console.log('Data de aniversário : ', dataAniversario)
+
+// idade depois que fiz aniversário
+idade = 27
+
+// imprimindo minha idade atual depois do aniversário 
+console.log('Minha idade depois do aniversário: ', idade)
